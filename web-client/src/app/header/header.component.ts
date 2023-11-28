62b7bf7e51500fc4755b87470b05cfaf15331319
ykucukcinar <yigitkucukcinar@hotmail.com>
2023-11-28 16:17:22 +0300

    Add view post function

--- web-client/src/app/header/header.component.ts
+++ web-client/src/app/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import { faUser } from '@fortawesome/free-solid-svg-icons';
-// import { AuthService } from '../auth/shared/auth.service';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from '../auth/shared/auth.service';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,25 +10,17 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
   faUser = faUser;
-  isLoggedIn: boolean;
-  username: string;
+  isLoggedIn: boolean = false;
+  username!: string;
 
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
-    // this.authService.loggedIn.subscribe((data: boolean) => this.isLoggedIn = data);
-    // this.authService.username.subscribe((data: string) => this.username = data);
-    // this.isLoggedIn = this.authService.isLoggedIn();
-    // this.username = this.authService.getUserName();
+    this.isLoggedIn = this.authService.isLoggedIn();
+    this.username = this.authService.getUserName();
   }
 
   goToUserProfile() {
     this.router.navigateByUrl('/user-profile/' + this.username);
-  }
-
-  logout() {
-    //this.authService.logout();
-    this.isLoggedIn = false;
-    this.router.navigateByUrl('');
   }
 }
